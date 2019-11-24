25150a3dab16e7893a6be3c55a08046d7586c01e
Saul Ordonez <sauleordonez@gmail.com>
2019-11-23 17:01:53 -0800

    seperate tests to accurately run individual tests

--- __tests__/galaxy-calculator.test.js
+++ __tests__/galaxy-calculator.test.js
@@ -9,38 +9,100 @@ describe('Earth Age', () => {
 
 });
 
-describe('Planet Age', () => {
+describe('Mercury Age', () => {
 
-    test('should return users age in specified planet years', () => {
+    test('should return users age in specified mercury years', () => {
       let planetAge = new PlanetAge(28);
       expect(planetAge.calculateMercuryAge()).toEqual(116.67);
+    });
+});
+
+describe('Venus Age', () => {
+
+    test('should return users age in specified venus years', () => {
+      let planetAge = new PlanetAge(28);
       expect(planetAge.calculateVenusAge()).toEqual(45.16);
+    });
+});
+
+describe('Mars Age', () => {
+
+    test('should return users age in specified mars years', () => {
+      let planetAge = new PlanetAge(28);
       expect(planetAge.calculateMarsAge()).toEqual(14.89);
+    });
+});
+
+describe('Jupiter Age', () => {
+
+    test('should return users age in specified jupiter years', () => {
+      let planetAge = new PlanetAge(28);
       expect(planetAge.calculateJupiterAge()).toEqual(2.36);
     });
-
 });
 
-describe('Planet Years Left', () => {
 
-    test('should return users remaining years left for specified planet', () => {
+describe('Mercury Years Left', () => {
+
+    test('should return users remaining years left for mercury', () => {
       let planetYearsLeft = new PlanetAge(28);
       expect(planetYearsLeft.calculateYearsLeftMercury()).toEqual(183.33);
-      expect(planetYearsLeft.calculateYearsLeftVenus()).toEqual(70.97);
-      expect(planetYearsLeft.calculateYearsLeftMars()).toEqual(23.40);
-      expect(planetYearsLeft.calculateYearsLeftJupiter()).toEqual(3.71);
     });
-
 });
 
-describe('Planet Years Over LEB', () => {
+describe('Venus Years Left', () => {
 
-    test('should return users years over the LEB for specified planet', () => {
+    test('should return users remaining years left for venus', () => {
+      let planetYearsLeft = new PlanetAge(28);
+      expect(planetYearsLeft.calculateYearsLeftVenus()).toEqual(70.97);
+    });
+});
+
+describe('Mars Years Left', () => {
+
+    test('should return users remaining years left for mars', () => {
+      let planetYearsLeft = new PlanetAge(28);
+      expect(planetYearsLeft.calculateYearsLeftMars()).toEqual(23.40);
+    });
+});
+
+describe('Jupiter Years Left', () => {
+
+    test('should return users remaining years left for jupiter', () => {
+      let planetYearsLeft = new PlanetAge(28);
+      expect(planetYearsLeft.calculateYearsLeftJupiter()).toEqual(3.71);
+    });
+});
+
+
+describe('Mercury Years Over LEB', () => {
+
+    test('should return users years over the LEB for mercury', () => {
       let planetYearsOver = new PlanetAge(85);
       expect(planetYearsOver.calculateYearsOverMercury()).toEqual(54.17);
+    });
+});
+
+describe('Venus Years Over LEB', () => {
+
+    test('should return users years over the LEB for venus', () => {
+      let planetYearsOver = new PlanetAge(85);
       expect(planetYearsOver.calculateYearsOverVenus()).toEqual(20.97);
+    });
+});
+
+describe('Mars Years Over LEB', () => {
+
+    test('should return users years over the LEB for mars', () => {
+      let planetYearsOver = new PlanetAge(85);
       expect(planetYearsOver.calculateYearsOverMars()).toEqual(6.91);
+    });
+});
+
+describe('Jupiter Years Over LEB', () => {
+
+    test('should return users years over the LEB for jupiter', () => {
+      let planetYearsOver = new PlanetAge(85);
       expect(planetYearsOver.calculateYearsOverJupiter()).toEqual(1.1);
     });
-
 });
